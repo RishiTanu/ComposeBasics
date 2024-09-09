c897fdeeaed1060e4b8ab3807afc9b200fb32a1f
RishiTanu <singhrishikesh500@gmail.com>
2024-09-09 10:30:38 +0530

    Added ImageCard, AppendText, Cards,Column, Rows

--- app/src/main/java/com/example/composebasics/MainActivity.kt
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -4,44 +4,212 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composebasics.ui.theme.ComposeBasicsTheme
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ComposeBasicsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            fontsText()
+        }
+    }
+}
+
+@Composable
+fun ImageCard(){
+    val painter = painterResource(id = R.drawable.appstore)
+    val description = "kermit playing in the snow"
+    val title = "kermit playing in the snow"
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(15.dp)
+    ) {
+        ImageCard(painter = painter, contentDescription = description, title = title)
+    }
+}
+
+@Composable
+fun fontsText() {
+    val fontFamily = FontFamily(
+        Font(R.font.firasans_bold, FontWeight.Thin),
+        Font(R.font.firasans_boldltalic, FontWeight.Thin)
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green)
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Red,
+                        fontSize = 50.sp
                     )
+                ) {
+                    append("J")
                 }
+                append("etpack ")
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Red,
+                        fontSize = 50.sp
+                    )
+                ) {
+                    append("C")
+                }
+                append("ompose")
+            },
+            color = Color.White,
+            fontSize = 30.sp,
+            fontFamily = fontFamily,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Center,
+            textDecoration = TextDecoration.Underline
+        )
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        ),
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 16.sp
+                    )
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Series1() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text("Start")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeBasicsTheme {
-        Greeting("Android")
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text("Start")
+    }
+
+    Column(
+        modifier = Modifier
+            .background(Color.Green)
+            .fillMaxHeight(0.5f)
+            .fillMaxWidth()
+            .padding(16.dp)
+            .border(5.dp, color = Color.Red)
+            .padding(5.dp)
+            .border(5.dp, color = Color.Blue)
+            .padding(5.dp)
+            .border(5.dp, color = Color.DarkGray)
+            .padding(15.dp)
+        //.width(600.dp)
+        //.requiredWidth(600.dp)
+    ) {
+
+        Text(
+            text = "Hello", modifier = Modifier
+                .border(5.dp, Color.Yellow)
+                .padding(5.dp)
+                .offset(10.dp, 10.dp)
+                .border(10.dp, Color.Blue)
+                .padding(10.dp)
+                .clickable {
+
+                }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "World")
     }
 }
