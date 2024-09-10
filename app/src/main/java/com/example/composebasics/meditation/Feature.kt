6a1d689075ac574c17cd105df9270d2fad555e21
RishiTanu <singhrishikesh500@gmail.com>
2024-09-11 01:40:10 +0530

    Medidation page

--- app/src/main/java/com/example/composebasics/meditation/Feature.kt
+++ app/src/main/java/com/example/composebasics/meditation/Feature.kt
@@ -5,7 +5,8 @@ import androidx.compose.ui.graphics.Color
 
 data class Feature(
     val title: String,
-    @DrawableRes val iconId: Color,
+    @DrawableRes val iconId: Int,
     val lightColor: Color,
+    val mediumColor: Color,
     val darkColor: Color,
 )
